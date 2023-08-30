a4f72755be476d3b06b613b45ffb901dcc9a417d
juanppjaime <j.penaj@uniandes.edu.co>
2023-08-30 18:13:03 -0500

    Arreglo reconocimiento

--- P0.py
+++ P0.py
@@ -5,23 +5,23 @@ from pyparsing import *
 num = '0' or  '1' or '2'
 ssn = num + '-' + num + '-' + num
 '''
-
+numeros = oneOf(["1","2","3","4","5","6","7","8","9","0"])
 dash = '-'
-'nums y existe'
-dir = Literal['left','right','front','back']
-ori = Literal['south','north','west','east']
+'numeros y existe'
+dir = oneOf(['left','right','front','back'])
+ori = oneOf(['south','north','west','east'])
 nombre = Word(alphanums) 
 
-valor = nums | dir | ori
-param =  '(' + ((valor) + ZeroOrMore(Literal(",") + (valor))) | Empty + ')'
-LVacia = ["nop("]
-LSimpleValor = ["walk(","leap(","drop(","get(","grab(","letGo("]
-LSimpleDir = ["turn("]
-LSimpleOri = ["turnto("]
-LDobleValorValor =  ["jump("]
-LDobleValorDir =  ["walk(","leap("]
-LDobleValorOri =  ["leap("]
-comando = (LVacia | LSimpleValor + nums | LSimpleDir + dir | LSimpleOri + ori | LDobleValorValor + nums + ',' + nums | LDobleValorDir + nums + ',' + dir | LDobleValorOri + nums + ',' + ori ) + ')'  
+valor = numeros | dir | ori
+param =  '(' + ((valor) + ZeroOrMore(Literal(",") + (valor))) | Empty() + ')'
+LVacia = oneOf(["nop("])
+LSimpleValor = oneOf(["walk(","leap(","drop(","get(","grab(","letGo("])
+LSimpleDir = oneOf(["turn("])
+LSimpleOri = oneOf(["turnto("])
+LDobleValorValor =  oneOf(["jump("])
+LDobleValorDir =  oneOf(["walk(","leap("])
+LDobleValorOri =  oneOf(["leap("])
+comando = (LVacia | (LSimpleValor + numeros) | (LSimpleDir + dir) | (LSimpleOri + ori) | (LDobleValorValor + numeros + ',' + numeros) | (LDobleValorDir + numeros + ',' + dir) | (LDobleValorOri + numeros + ',' + ori )) + ')'  
 
 #Condiciones
 facing = 'facing(' + ori + ')' 
@@ -31,7 +31,7 @@ nott = 'not:' + facing | can
 condicion = facing | can | nott
 
 # Bloque
-bloque = '{' + ((comando) + ZeroOrMore(Literal(";") + (comando))) | Empty + '}'
+bloque = '{' + ((comando) + ZeroOrMore(Literal(";") + (comando))) | Empty() + '}'
 
 #IF
 iff = 'if' + condicion + bloque + 'else' + bloque
@@ -40,10 +40,10 @@ iff = 'if' + condicion + bloque + 'else' + bloque
 whilee = 'while' + condicion + bloque
 
 #Repeat
-repeat = 'repeat' + nums + 'times' + bloque
+repeat = 'repeat' + numeros + 'times' + bloque
 
 #Defvar
-defVar = 'defVar' + nombre + nums
+defVar = 'defVar' + nombre + numeros
 
 #DefProc
 DefProc = 'defProc' + nombre + param + bloque
@@ -51,17 +51,43 @@ DefProc = 'defProc' + nombre + param + bloque
 #Llamado
 llamado = bloque
 
-ssn_parser = Combine(
-  Word(nums, exact=3)
-  + dash
-  + Word(nums, exact=2)
-  + dash
-  + Word(nums, exact=4)
-)   
-
-input_string = """
-  xxx 225-92-8416 yyy
-  103-33-3929 zzz 028-91-0122
+pattern = ZeroOrMore(defVar) + ZeroOrMore(DefProc)
+input_text = """
+defVar nom 1
+defVar y 0
+defVar one 0
+defProc putCB (5 , north )
+{
+drop(5);
+letGo (3);
+walk(5)
+}
+defProc goNorth()
+{
+while can(walk(1 , north ) ) { walk(1 , north ) }
+}
+defProc goWest ()
+{
+    if can(walk(1 , west ) ) { walk(1 , west ) } e l s e nop ()
+}
+{
+jump (3 ,3) ;
+putCB (2 ,1)
+}
 """
+
+# Analizar una cadena
+result = pattern.parseString(input_text)
+if result:
+    print("La palabra 'sigue' sigue a 'esto'")
+else:
+    print("La palabra 'sigue' no sigue a 'esto'")
+
+for match, start, stop in pattern.scanString(input_text):
+  print(match,start,match)
+
+
+
+  
 
             
