4e429cb792d5eaf4cb4688d5df94eb200fda2b44
Camilo Daza Portatil <camilodaza-96@hotmail.com>
2023-08-30 17:40:06 -0500

    Avance 2 codigo

--- import pyparsing as pp.py
+++ import pyparsing as pp.py
@@ -8,20 +8,20 @@ ssn = num + '-' + num + '-' + num
 
 dash = '-'
 'nums y existe'
-dir = Literal['left','right','front','back']
-ori = Literal['south','north','west','east']
+dir = oneOf(['left','right','front','back'])
+ori = oneOf(['south','north','west','east'])
 nombre = Word(alphanums) 
 
-valor = Word
-param =  '(' + ((valor) + ZeroOrMore(Literal(",") + (valor))) | Empty + ')'
-LVacia = ["nop("]
-LSimpleValor = ["walk(","leap(","drop(","get(","grab(","letGo("]
-LSimpleDir = ["turn("]
-LSimpleOri = ["turnto("]
-LDobleValorValor =  ["jump("]
-LDobleValorDir =  ["walk(","leap("]
-LDobleValorOri =  ["leap("]
-comando = (LVacia | LSimpleValor + nums | LSimpleDir + dir | LSimpleOri + ori | LDobleValorValor + nums + ',' + nums | LDobleValorDir + nums + ',' + dir | LDobleValorOri + nums + ',' + ori ) + ')'  
+valor = nums | dir | ori
+param =  '(' + ((valor) + ZeroOrMore(Literal(",") + (valor))) | Empty() + ')'
+LVacia = oneOf(["nop("])
+LSimpleValor = oneOf(["walk(","leap(","drop(","get(","grab(","letGo("])
+LSimpleDir = oneOf(["turn("])
+LSimpleOri = oneOf(["turnto("])
+LDobleValorValor =  oneOf(["jump("])
+LDobleValorDir =  oneOf(["walk(","leap("])
+LDobleValorOri =  oneOf(["leap("])
+comando = (LVacia | (LSimpleValor + nums) | (LSimpleDir + dir) | (LSimpleOri + ori) | (LDobleValorValor + nums + ',' + nums) | (LDobleValorDir + nums + ',' + dir) | (LDobleValorOri + nums + ',' + ori )) + ')'  
 
 #Condiciones
 facing = 'facing(' + ori + ')' 
@@ -31,7 +31,7 @@ nott = 'not:' + facing | can
 condicion = facing | can | nott
 
 # Bloque
-bloque = '{' + ((comando) + ZeroOrMore(Literal(";") + (comando))) | Empty + '}'
+bloque = '{' + ((comando) + ZeroOrMore(Literal(";") + (comando))) | Empty() + '}'
 
 #IF
 iff = 'if' + condicion + bloque + 'else' + bloque
@@ -43,7 +43,7 @@ whilee = 'while' + condicion + bloque
 repeat = 'repeat' + nums + 'times' + bloque
 
 #Defvar
-defVar = 'defVar' + nombre + nums
+defVar = 'defVar' + 'nom' + (nums)
 
 #DefProc
 DefProc = 'defProc' + nombre + param + bloque
@@ -52,20 +52,39 @@ DefProc = 'defProc' + nombre + param + bloque
 llamado = bloque
 
 ssn_parser = Combine(
-  Word(nums, exact=3)
-  + dash
-  + Word(nums, exact=2)
-  + dash
-  + Word(nums, exact=4)
+ ZeroOrMore(defVar)
+ +
+ ZeroOrMore(DefProc)
+ +
+ llamado
 )   
 
-input_string = """
-  xxx 225-92-8416 yyy
-  103-33-3929 zzz 028-91-0122
+input_string = """defVarnom1
+defVar nom 1
+defVar y 0
+defVar one 0
+defProc putCB (5 , north )
+{
+drop(5);
+letGo (3);
+walk(5)
+}
+defProc goNorth()
+{
+while can(walk(1 , north ) ) { walk(1 , north ) }
+}
+defProc goWest ()
+{
+    if can(walk(1 , west ) ) { walk(1 , west ) } e l s e nop ()
+}
+{
+jump (3 ,3) ;
+putCB (2 ,1)
+}
 """
 
 for match, start, stop in ssn_parser.scanString(input_string):
-  print(match)
+  print(match,start,match)
   
   
 
