e283b508e9213b56051e33f5cf7be357b5a03b34
Camilo Daza Portatil <camilodaza-96@hotmail.com>
2023-08-30 17:04:04 -0500

    Inicio Taller0 LyM

--- P0.py
+++ P0.py
@@ -1,2 +1,17 @@
 
+
+comandos_simples = ["walk(","leap(","turn(","turnto","drop(","get(","grab(","letGo(","nop(",]
+valores_iniciales = ["0","1","2","3","4","5","6","7","8","9"]
+comandos_dobles = ["walk(","leap(","jump("]
+parentesis_cierre = [")"]
+parentesis_abrir = ["("]
+corchete_abrir = ["{"]
+corchete_cerrar = ["}"]
+coma=[","]
+comandos_normales = ["M","R","C","B","c","P"]
+declaraciones_variables = ["defVar"]
+declaraciones_procedimientos = ["defProc"]
+variables = ["nomb","ramb"]
+nombre_procedimiento = ["goNorth","PUTCB"]
+direcciones=["north", "south","west","east"]
             
