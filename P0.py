aa0c497ac2d55654e1f704cfff4dfcabc5bb2d83
juanppjaime <j.penaj@uniandes.edu.co>
2023-09-02 14:20:34 -0500

    Avance

--- P0.py
+++ P0.py
@@ -12,26 +12,31 @@ dir = oneOf(['left','right','front','back'])
 ori = oneOf(['south','north','west','east'])
 nombre = Word(alphanums) 
 
-valor = numeros | dir | ori
-param =  '(' + ((valor) + ZeroOrMore(Literal(",") + (valor))) | Empty() + ')'
+valor = Word(alphanums)
+param =  ('(' + ((valor) + ZeroOrMore(Literal(",") + (valor))) + ')') | "()"
 LVacia = oneOf(["nop("])
 LSimpleValor = oneOf(["walk(","leap(","drop(","get(","grab(","letGo("])
 LSimpleDir = oneOf(["turn("])
 LSimpleOri = oneOf(["turnto("])
 LDobleValorValor =  oneOf(["jump("])
 LDobleValorDir =  oneOf(["walk(","leap("])
-LDobleValorOri =  oneOf(["leap("])
-comando = (LVacia | (LSimpleValor + numeros) | (LSimpleDir + dir) | (LSimpleOri + ori) | (LDobleValorValor + numeros + ',' + numeros) | (LDobleValorDir + numeros + ',' + dir) | (LDobleValorOri + numeros + ',' + ori )) + ')'  
+LDobleValorOri =  oneOf(["leap(","walk("])
+comando = (LVacia + ")" | (LSimpleValor + numeros + ")") | (LSimpleDir + dir + ")") | (LSimpleOri + ori + ")") | (LDobleValorValor + numeros + ',' + numeros + ")") | (LDobleValorDir) + numeros + "," + dir + ")" | (LDobleValorOri + numeros + ',' + ori + ")") ) 
+comando2 = (LVacia  | (LSimpleValor + valor) | (LSimpleDir + valor) | (LSimpleOri + valor) | (LDobleValorValor + valor + ',' + valor) | (LDobleValorDir + valor + ',' + valor) | (LDobleValorOri + valor + ',' + valor )) + ')'   
 
 #Condiciones
 facing = 'facing(' + ori + ')' 
-can = 'can(' + comando + ')'
+can = 'can(' + comando + ')' 
 nott = 'not:' + facing | can
 
 condicion = facing | can | nott
 
-# Bloque
-bloque = '{' + ((comando) + ZeroOrMore(Literal(";") + (comando))) | Empty() + '}'
+# Bloque con numeros , dir u ori
+bloque = '{' + ((comando) + ZeroOrMore(Literal(";") + (comando))) + '}'
+
+#Bloque con parametros
+
+bloque2 = '{' + ((comando2) + ZeroOrMore(Literal(";") + (comando2))) + '}'
 
 #IF
 iff = 'if' + condicion + bloque + 'else' + bloque
@@ -42,40 +47,44 @@ whilee = 'while' + condicion + bloque
 #Repeat
 repeat = 'repeat' + numeros + 'times' + bloque
 
+#Bloque condicional
+bloque_condicional = '{' + iff | whilee | repeat + '}'
+
 #Defvar
 defVar = 'defVar' + nombre + numeros
 
 #DefProc
-DefProc = 'defProc' + nombre + param + bloque
+defProc = 'defProc' + nombre + param + ZeroOrMore(iff|bloque2) 
 
 #Llamado
 llamado = bloque
 
-pattern = ZeroOrMore(defVar) + ZeroOrMore(DefProc)
+pattern = ZeroOrMore(defVar) + ZeroOrMore(defProc) 
 input_text = """
 defVar nom 1
 defVar y 0
 defVar one 0
-defProc putCB (5 , north )
+defProc putCB (a,n)
 {
 drop(5);
-letGo (3);
+letGo(3);
 walk(5)
 }
 defProc goNorth()
+ifcan(walk(1,west)){walk(1,west)}elsenop()
+defProc goWest (6,south)
 {
-while can(walk(1 , north ) ) { walk(1 , north ) }
+drop(5);
+letGo(3);
+walk(9)
 }
-defProc goWest ()
+defProc goWest (6,south)
 {
-    if can(walk(1 , west ) ) { walk(1 , west ) } e l s e nop ()
-}
-{
-jump (3 ,3) ;
-putCB (2 ,1)
+jump (3,3);
+putCB (2,1)
 }
 """
-
+#input_text = input_text.replace(" ","")
 # Analizar una cadena
 result = pattern.parseString(input_text)
 if result:
